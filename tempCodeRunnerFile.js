9624713a81d54314691e9ac40ae7df547a3388a2
Arty <a.barbonov@aknet.kg>
2019-04-02 11:45:03 +0600

    Мой первый комит

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+let costs1 = prompt("Введите обязательную статью расходов в этом месяце");
+let costs2 = prompt("Введите обязательную статью расходов в этом месяце");
+let question1 = prompt("Во сколько обойдется?");
+let question2 = prompt("Во сколько обойдется?");
+let expenses={
+    costs1:costs2,
+    question1:question2
+}
+console.log(expenses)
