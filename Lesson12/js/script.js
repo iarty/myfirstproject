68b5e4ddacd36f8d32037c1ae4bc14600ebfb268
Arty <a.barbonov@aknet.kg>
2019-04-15 13:37:04 +0600

    lesson12

--- Lesson12/js/script.js
+++ Lesson12/js/script.js
@@ -1,50 +1,31 @@
-
-
-// inputRub.addEventListener('input', () => {
-    // let request = new XMLHttpRequest();
-
-    // request.open('GET', 'js/current.json');
-    // request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    // request.send();
-
-    // request.addEventListener('readystatechange', function () {
-    //     if (request.readyState === 4 && request.status == 200) {
-    //         let data = JSON.parse(request.response);
-
-    //         inputUsd.value = inputRub.value / data.usd;
-    //     } else {
-    //         inputUsd.value = "Что-то пошло не так!";
-    //     }
-    // });
-
-// });
-
 let inputRub = document.getElementById('rub'),
-    inputUsd = document.getElementById('usd');
+    inputUsd = document.getElementById('usd'),
+    btn = document.getElementById('convert');
 
-function converter(rub,usd){
-    return new Promise(function (good, notgood) {
-    let request = new XMLHttpRequest();
-    request.open('GET', 'js/current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    request.send();
-    request.addEventListener('readystatechange', function () {
-        if (request.readyState === 4 && request.status == 200) {
-            good(()=>{
-                let data = JSON.parse(request.response);
-                usd.value = rub.value / data.usd;
-            });
-        } else {
-            notgood(()=>{
-                usd.value = "Что-то пошло не так!";
-            });
-        }
-    });
+btn.addEventListener('click', () => {
 
+    function getData() {
+        return new Promise(function (resolve, reject) {
+            let request = new XMLHttpRequest();
+            request.open('GET', 'js/current.json');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            request.onload = function () {
+                if (request.readyState === 4 && request.status == 200) {
+                    let data = JSON.parse(request.response);
+                    resolve(data);
+                } else {
+                    reject();
+                }
+            };
+            request.send();
+        });
+    };
+
+    getData()
+        .then((data) => {
+            inputUsd.value = inputRub.value / data.usd;
+        })
+        .catch(() => {
+            inputUsd.value = "Что-то пошло не так!";
+        });
 });
-};
-
-
-converter(inputRub,inputUsd)
-    .then(good)
-    .catch(notgood);
